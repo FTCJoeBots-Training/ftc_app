44f2741396dd444e6f6d705b1b5886d4280affac
LPrice123 <pizzaprice6@gmail.com>
2017-10-07 09:20:43 -0400

    Updated color sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skeleton_OP.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by 12768 on 10/4/17.
+ */
+
+public class Skeleton_OP {
+}
+@Override
+public void init() {
+
+}
+@Override
+public void start(){
+
+}
+@Override
+public void loop(){
+
+}
+
+@Override
+public void stop(){
+
+}
