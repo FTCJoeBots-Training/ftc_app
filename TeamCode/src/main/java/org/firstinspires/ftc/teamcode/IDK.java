a794ad21a8e28edcedd5f76f1e20eefa9d392432
CVaughn123 <charliev999@gmail.com>
2017-10-11 19:35:34 -0400

    Updated Sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IDK.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IDK.java
@@ -1,17 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by 535540 on 10/11/17.
- */
-//IDK what to call this so IDK will do... Don't ever question me.
-public class IDK {
-}
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-{
-    public void runOpMode() throws InterruptedException{
+@TeleOp (name ="TankToAll", group = "TeleOp")
+@Disabled
+//IDK what to call this so IDK will do... Don't ever question me.
+public class IDK extends LinearOpMode {
+
+    HardwareJoeBot robot = new HardwareJoeBot();
+
+
+
+        @Override
+        public void runOpMode () throws InterruptedException {
         double left;
         double right;
-
+        double rightTrigger;
+        double leftTrigger;
+        double rPosition;
+        double lPosition;
 
         //double max;
 
@@ -20,23 +29,31 @@ public class IDK {
          */
         robot.init(hardwareMap);
 
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say","Hello Ethan");    //
-        telemetry.update();
-
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
         // run until the end of the match (driver presses STOP)
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
 
-        // Run wheels in Tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-        // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // Run wheels in Tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            left = -gamepad1.left_stick_y;
+            right = -gamepad1.right_stick_y;
+            robot.motor_driveleft.setPower(left);
+            robot.motor_driveright.setPower(right);
 
-        left=-gamepad1.left_stick_y/-1.4;
-        right=-gamepad1.right_stick_y/-1.5;
-        robot.motor_driveleft.setPower(left);
-        robot.motor_driveright.setPower(right);
+            // Raise Arm with Triggers
+            leftTrigger = gamepad1.left_trigger;
+            rightTrigger = gamepad1.right_trigger;
+            //If the right trigger is pressed, we're going to ignore left trigger
+            if (rightTrigger>0) {
+                // robot.motor_arm.setPower(rightTrigger);
+            } else if (leftTrigger>0) {
+                // robot.motor_arm.setPower(-leftTrigger);
+            } else {
+                //robot.motor_arm.setPower(0);
+            }
+
+
         }
-        }
+
+
+
+    }
