4074a5103d5df3b005e80d81a3408e56e3127db2
CVaughn123 <charliev999@gmail.com>
2017-12-09 11:48:40 -0500

    Inputed the correct correspoding code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingGround/autoBlueOneDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingGround/autoBlueOneDrive.java
@@ -340,6 +340,10 @@ public class autoBlueOneDrive extends LinearOpMode {
             stopmotors();
         }
 
+
+
+
+
         //---------------------------------------------------------------------------------//;
 
 
